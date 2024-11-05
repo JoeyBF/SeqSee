6251b0b7658d69b54c633ba4947d22b3cb87692f
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2024-11-05 15:48:11 -0500

    Initial commit

---
+++ devenv.nix
@@ -0,0 +1,53 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    poetry = {
+      enable = true;
+      install = {
+        enable = true;
+        installRootPackage = false;
+        onlyInstallRootPackage = false;
+        compile = false;
+        quiet = false;
+        groups = [ ];
+        ignoredGroups = [ ];
+        onlyGroups = [ ];
+        extras = [ ];
+        allExtras = false;
+        verbosity = "no";
+      };
+      activate.enable = true;
+      package = pkgs.poetry;
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
