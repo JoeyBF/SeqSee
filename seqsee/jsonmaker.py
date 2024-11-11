4f77cf19dbcc82346be57d30081910227c82deea
Joey Beauvais-Feisthauer <joeybf@wayne.edu>
2024-11-11 03:19:03 -0500

    QoL updates to jsonmaker

--- seqsee/jsonmaker.py
+++ seqsee/jsonmaker.py
@@ -1,10 +1,24 @@
 import json
 import pandas as pd
 import re
+import sys
 from .main import load_schema
 from collections import defaultdict
 from compact_json import Formatter
 from jsonschema import validate
+
+
+# Match the words 'D' and 't', respectively
+substitute_Delta = re.compile(r"\bD\b")
+substitute_tau = re.compile(r"\bt\b")
+# Matches words starting with latin letters (group 1) and
+# ending with numbers or a comma (,) (group 2).
+# NB: Caret (^) is a word separator.
+make_subscript = re.compile(r"\b([a-zA-Z]+)([\d,]+)\b")
+# Matches terms of the form '(letters)^(numbers)'
+make_exponents = re.compile(r"\b([a-zA-Z]+)\^(\d+)\b")
+
+arrow_length = 0.7
 
 
 def extract_node_attributes(row):
@@ -39,20 +53,50 @@ def extract_edge_attributes(row, edge_type, nodes):
     return ret
 
 
+def label_from_node_name(node_name):
+    # Regular expressions working their magic:
+    label = substitute_Delta.sub(r"\\Delta", node_name)
+    label = substitute_tau.sub(r"\\tau", label)
+    label = make_subscript.sub(r"\1_{\2}", label)
+    label = make_exponents.sub(r"\1^{\2}", label)
+    return label
+
+
+def deduplicate_name(name):
+    """Deduplicate names by removing the "again" suffix. We also return whether the name was a
+    duplicate."""
+    # I suggest we change the csv format to remove the "again" suffixes, and instead have a
+    # semicolon-separated list of names for the targets of the edges. However, the input is
+    # from a legacy dataset, so I don't have control over that.
+    if "again" in name:
+        return (name.replace(" again2", "").replace(" again", ""), True)
+    return (name, False)
+
+
 def main():
+    if len(sys.argv) != 3:
+        print("Usage: jsonmaker <input.csv> <output.json>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
     # Define the JSON schema
     schema = load_schema()
 
     # Load CSV data
-    df = pd.read_csv("Adams-motivic-E2.csv")
+    df = pd.read_csv(input_file)
 
     # Build the JSON object based on the schema
     json_data = {
         "header": {
-            "title": "The $E_2$-page of the motivic Adams spectral sequence",
-            "dimensions": {"width": 120, "height": 60},
+            "chart": {
+                "title": "The $E_2$-page of the motivic Adams spectral sequence",
+                "width": 120,
+                "height": 60,
+            },
             "defaultAttributes": {
-                "nodes": [{"color": "gray", "shape": "circle", "size": "0.03"}],
+                "nodes": [{"color": "gray", "shape": "circle"}],
                 "edges": [{"color": "gray", "thickness": "0.02", "lineStyle": "solid"}],
             },
             "aliases": {
@@ -90,7 +134,11 @@ def main():
     # Process nodes first
     for _, row in df.iterrows():
         # Process node information
-        node_name = row["name"]
+
+        node_name, is_duplicate = deduplicate_name(row["name"])
+        if is_duplicate:
+            continue
+
         x = int(row["stem"])
         y = int(row["Adams filtration"])
         bidegree_to_rank[x, y] += 1
@@ -98,7 +146,7 @@ def main():
             "x": x,
             "y": y,
             "position": bidegree_to_rank[x, y] - 1,
-            "label": node_name,
+            "label": label_from_node_name(node_name),
         }
         if attributes := extract_node_attributes(row):
             # Only add an attributes key if there are attributes to add
@@ -106,7 +154,7 @@ def main():
 
     # Process edges after, since they depend on nodes
     for _, row in df.iterrows():
-        node_name = row["name"]
+        node_name, _ = deduplicate_name(row["name"])
         for edge_type in ["h0", "h1", "h2", "dr"]:
             target_col = f"{edge_type}target"
             target_info = f"{edge_type}info"
@@ -123,11 +171,11 @@ def main():
                 elif target_node == "loc":
                     # This is an arrow
                     if edge_type == "h0":
-                        x_offset = 0 * 0.7
-                        y_offset = 1 * 0.7
+                        x_offset = 0 * arrow_length
+                        y_offset = 1 * arrow_length
                     elif edge_type == "h1":
-                        x_offset = 1 * 0.7
-                        y_offset = 1 * 0.7
+                        x_offset = 1 * arrow_length
+                        y_offset = 1 * arrow_length
                     else:
                         raise ValueError
                     edge_data["offset"] = {"x": x_offset, "y": y_offset}
@@ -139,8 +187,8 @@ def main():
             elif row[target_info] == "free":
                 # This is also an arrow, but with a different notation
                 if edge_type == "dr":
-                    x_offset = -1 * 0.7
-                    y_offset = 2 * 0.7
+                    x_offset = -1 * arrow_length
+                    y_offset = 2 * arrow_length
                 else:
                     raise ValueError
                 edge_data["offset"] = {"x": x_offset, "y": y_offset}
@@ -161,7 +209,7 @@ def main():
         validate(instance=json_data, schema=schema)
         formatter = Formatter()
         formatter.indent_spaces = 2
-        formatter.dump(json_data, "Adams-motivic-E2.json")
+        formatter.dump(json_data, output_file)
         print("JSON data successfully generated and validated against the schema.")
     except Exception as e:
         print("Validation error:", e)
